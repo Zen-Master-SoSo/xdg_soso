5104da2e6d173fa4e964bb1eb5575afe0a5ca8e7
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-07-22 09:38:57 +0800

    Bump version 0.2.0 -> 0.3.0

--- xdg_soso/__init__.py
+++ xdg_soso/__init__.py
@@ -30,7 +30,7 @@ from xml.etree.ElementTree import Element
 from tempfile import mkstemp
 from subprocess import run, CalledProcessError
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def is_xdg():
